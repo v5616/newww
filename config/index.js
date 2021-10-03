119caa8cc9dbd65becc403e6e418686ae81f7508
Vishal Rajput <63779755+v5616@users.noreply.github.com>
2021-10-03 09:42:07 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXTJS_PUBLIC_URL || "https://admin-freshers.herokuapp.com";
+export const API_URL = process.env.NEXTJS_PUBLIC_URL || "https://admin-backendd.herokuapp.com";
